c7bc4c1650b60682ee8eca3326be5c739ad428ab
Jay Chaturvedi <jaychaturvedi18@gmail.com>
2023-06-21 22:10:14 +0530

    react router setup and package installation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,39 @@
+import "./App.css";
+import { ToastContainer } from "react-toastify";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Layout/Navbar";
+import LoginRegistrationForm from "./components/Forms/LoginRegistrationForm";
+
 function App() {
   return (
     <div className="App">
+      <Navbar />
+      {/* A <Switch> looks through its children <Route>s and
+          renders the first one that matches the current URL. */}
+      <Routes>
+        <Route path="/" element={<div>/</div>} />
+        <Route
+          path="/login"
+          element={<LoginRegistrationForm currentTab="login" />}
+        />
+        <Route
+          path="/signup"
+          element={<LoginRegistrationForm currentTab="signup" />}
+        />
+      </Routes>
+
+      {/* toast notification for api calls */}
+      <ToastContainer
+        position="top-right"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
     </div>
   );
 }
